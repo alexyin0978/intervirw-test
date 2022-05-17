8743cd4ebe5077ce05b365acf2ee9cd985b6d0c9
Yin <matt3982@Yinde-MacBook-Pro-2.local>
2022-05-17 19:43:18 +0800

    upload all test files to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+import jsonObj from './nested.json';
+
+
+const Iterate = ({obj}) => {
+
+  if(obj){
+
+    const [id,type] = Object.keys(obj);
+  
+    if(typeof obj[type] === 'object'){
+  
+      const arr = obj[type];
+  
+      return(
+        <div className={arr[0]} key={obj[id]}>
+          {arr[0]}
+          <>
+            {
+             arr.filter((subObj,idx) => idx > 0)
+             .map(subObj => {
+              const [id] = Object.keys(subObj);
+               return (
+                <Iterate obj={subObj} key={subObj[id]}/>
+               )
+             })
+            }
+          </>
+        </div>
+      )
+    }else{
+      return(
+        <div className={obj[type]} key={obj[id]}>
+          {obj[type]}
+        </div>
+      )
+    }
+  }
+};
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        jsonObj ? <Iterate obj={jsonObj} /> : null
+      }
+    </>
   );
 }
 
 export default App;
+
+// const Iterate = (obj) => {
+//   Object.keys(obj).forEach(key => {
+
+//   console.log(`key: ${key}, value: ${obj[key]}`)
+
+//   if (typeof obj[key] === 'object') {
+//           Iterate(obj[key])
+//       }
+//   })
+// }
+
+
+
+
+
+
